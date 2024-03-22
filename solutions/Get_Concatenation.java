8d6dd807ef9b1a96da9e461e604a9c8654547165
shihab-sani <mdsani633@gmail.com>
2024-03-22 20:05:10 +0600

    Get Concatenation

---
+++ solutions/Get_Concatenation.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int size = nums.length*2;
+        int[] arr = new int[size];
+        System.arraycopy(nums,0,arr,0,nums.length);
+        int j = nums.length;
+        for (int i = 0; i<nums.length; i++){
+            arr[j] = nums[i];
+            j++;
+        }
+        return arr;
+    }
+}
