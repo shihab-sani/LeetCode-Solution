48a3358844cc522cb29740146099ee2946036e72
shihab-sani <mdsani633@gmail.com>
2024-03-25 02:20:38 +0600

    Contains Duplicate II

---
+++ solutions/ContainsNearbyDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+class ContainsNearbyDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int left = 0;
+        HashSet<Integer> window = new HashSet<>();
+
+        for (int right = 0; right < nums.length; right++) {
+            if (window.size() > k) {
+                window.remove(nums[left]);
+                left++;
+            }
+            if (window.contains(nums[right])) 
+                return true;
+            window.add(nums[right]);
+        }
+        return false;
+    }
+}
