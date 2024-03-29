77d78c803c92781f16f6749387bab663c17404fe
shihab-sani <mdsani633@gmail.com>
2024-03-29 23:30:06 +0600

    Two Sum II - Input Array Is Sorted

---
+++ solutions/TwoSum.java
@@ -0,0 +1,20 @@
+class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        int left = 0;
+        int right = numbers.length-1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) 
+                right--;
+            else if (numbers[left] + numbers[right] < target)
+                left++;
+            else if (numbers[left] + numbers[right] == target){
+                result[0] = left+1;
+                result[1] = right+1;
+                return result;
+            }
+        }
+        return result;
+    }
+}
