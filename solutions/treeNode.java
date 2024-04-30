0ec4b6e7190a47810906f1e972f36be8cf5495c4
shihab-sani <mdsani633@gmail.com>
2024-04-30 11:59:36 +0600

    Search in a Binary Search Tree

---
+++ solutions/treeNode.java
@@ -0,0 +1,28 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+  }
+ 
+class treeNode {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return root;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return root;
+        }
+    }
+}
