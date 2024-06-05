06b3e83838727b1a5539942e02d731947f2649db
shihab-sani <mdsani633@gmail.com>
2024-06-05 22:06:56 +0600

    Number of Islands

---
+++ solutions/NumIslands.java
@@ -0,0 +1,27 @@
+class NumIslands {
+    public int numIslands(char[][] grid) {
+        int island = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    helper(grid, i, j);
+                    island++;
+                }
+            }
+        }
+        return island;
+    }
+
+    private void helper(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        helper(grid, i + 1, j);
+        helper(grid, i - 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i, j - 1);
+    }
+}
